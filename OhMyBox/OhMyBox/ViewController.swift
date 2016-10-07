a75bc1cd063764e2e8713885ab5c731692da12b2
thiagotmb <tmb0710@gmail.com>
2016-10-07 13:38:19 -0300

    Adding Xcode Project

---
+++ OhMyBox/OhMyBox/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  OhMyBox
+//
+//  Created by Thiago-Bernardes on 10/7/16.
+//  Copyright © 2016 Lab262. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
