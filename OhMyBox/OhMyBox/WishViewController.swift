f27741eecccc6a54f1fce18a1c428b0f7e9148f9
felipe <felipe.perius@gmail.com>
2016-10-19 15:45:57 -0200

    implemented tab wishlist front-end

---
+++ OhMyBox/OhMyBox/WishViewController.swift
@@ -0,0 +1,75 @@
+//
+//  WishViewController.swift
+//  OhMyBox
+//
+//  Created by Felipe perius on 19/10/16.
+//  Copyright © 2016 Lab262. All rights reserved.
+//
+
+import UIKit
+
+class WishViewController: UIViewController {
+
+    @IBOutlet weak var tableView: UITableView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.registerNib()
+       
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    func registerNib() {
+        
+        self.tableView.register(UINib(nibName: "WishTableViewCell", bundle: nil), forCellReuseIdentifier: WishTableViewCell.identifier)
+
+        
+    }
+    
+    func generateFavoriteCell(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let cell = tableView.dequeueReusableCell(withIdentifier: WishTableViewCell.identifier, for: indexPath) as! WishTableViewCell
+        cell.selectionStyle = .none
+        return cell
+    }
+ 
+}
+extension WishViewController: UITableViewDataSource {
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        
+        return generateFavoriteCell(tableView, cellForRowAt: indexPath)
+    }
+    
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        
+        return 5
+    }
+    
+   
+    
+}
+
+extension WishViewController: UITableViewDelegate {
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        
+    }
+    
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        
+  
+            return 360
+       
+        
+    }
+  
+    
+}
+
