7351b488cfaafb03783a0db6eb02c82142020962
Huallyd <huallyd@gmail.com>
2016-11-10 14:54:18 -0200

    Alamofire.

--- OhMyBox/OhMyBox/ApplicationState.swift
+++ OhMyBox/OhMyBox/ApplicationState.swift
@@ -17,7 +17,7 @@ import Alamofire
 #endif
 
 
-let URL_WS_SERVER = "https://leituradebolso.herokuapp.com/api/v0/"
+//let URL_WS_SERVER = "https://leituradebolso.herokuapp.com/api/v0/"
 let URL_WS_LOCAL = "http://localhost:8080/api/v0/"
 
 let TOKEN = ["token": "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6InRtYjA3MTBAZ21haWwuY29tIiwiaWQiOiI1N2U1YmIyNWU5MzgwNTAzMDBjMGI5MTQiLCJpYXQiOjE0NzU1MzczODV9.0f4Bm6rYgdiJWoCKMGJDNdKqKd9yCzLJ0NMxK3sGLkc"]
@@ -26,10 +26,10 @@ let TOKEN = ["token": "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6InRtYjA3
 
 class ApplicationState: NSObject {
     
-    var allReadings = [Reading]()
-    var favoriteReads = [Reading]()
-    var unreadReadings = [Reading]()
-    var currentUser: User?
+//    var allReadings = [Reading]()
+//    var favoriteReads = [Reading]()
+//    var unreadReadings = [Reading]()
+//    var currentUser: User?
     
     static let sharedInstance : ApplicationState = {
         let instance = ApplicationState(singleton: true)
@@ -40,7 +40,7 @@ class ApplicationState: NSObject {
     private init(singleton: Bool) {
         super.init()
         
-        self.currentUser = DBManager.getAll().first
+       // self.currentUser = DBManager.getAll().first
         
     }
 
