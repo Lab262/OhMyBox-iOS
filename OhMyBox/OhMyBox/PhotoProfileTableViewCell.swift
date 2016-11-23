0af8d2b5dbc67b198eec48a93cb6a29db4afa7c7
felipe <felipe.perius@gmail.com>
2016-11-23 04:07:16 -0200

    New layout profile

--- OhMyBox/OhMyBox/PhotoProfileTableViewCell.swift
+++ OhMyBox/OhMyBox/PhotoProfileTableViewCell.swift
@@ -11,8 +11,10 @@ import UIKit
 class PhotoProfileTableViewCell: UITableViewCell {
     static let identifier = "photoProfileCell"
 
+    @IBOutlet weak var parallaxConstraint: NSLayoutConstraint!
     @IBOutlet weak var photoProfileImage: UIImageView!
     @IBOutlet weak var nameProfileLabel: UILabel!
+    internal var lastContentOffset: CGFloat = 0
     
     override func awakeFromNib() {
         super.awakeFromNib()
@@ -26,4 +28,32 @@ class PhotoProfileTableViewCell: UITableViewCell {
         // Configure the view for the selected state
     }
       
+}
+
+extension PhotoProfileTableViewCell: UIScrollViewDelegate {
+    
+    func scrollViewDidScroll(_ scrollView: UIScrollView) {
+        
+        
+      
+        
+        if scrollView.contentOffset.y <= 0 {
+            self.parallaxConstraint.constant = self.parallaxConstraint.constant + (self.lastContentOffset - scrollView.contentOffset.y)
+        } else {
+            self.parallaxConstraint.constant = 223
+        }
+        
+        if self.parallaxConstraint.constant > 223 && scrollView.contentOffset.y >= 0  {
+            self.parallaxConstraint.constant = 223
+            UIView.animate(withDuration: 0.4, delay: 0.0, usingSpringWithDamping: 0.5, initialSpringVelocity: 0.5, options: .curveEaseInOut, animations: {
+                self.layoutIfNeeded()
+                }, completion: nil)
+        }
+        
+        
+        self.lastContentOffset = scrollView.contentOffset.y
+        
+    }
+    
+    
 }
