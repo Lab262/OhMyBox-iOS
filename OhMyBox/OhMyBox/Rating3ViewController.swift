cebd572f1f956e305d4a9b1b7976c4416acf6e8f
andreanmasiro <andreanmasiro@live.com>
2017-03-17 17:29:05 -0300

    feedback changes based on rating

--- OhMyBox/OhMyBox/Rating3ViewController.swift
+++ OhMyBox/OhMyBox/Rating3ViewController.swift
@@ -12,7 +12,16 @@ class Rating3ViewController: UIViewController {
 
     typealias Info = (image: UIImage, rating: Int)
     
-    static let thankYouString = "MUITO OBRIGADO!"
+    struct OutletStrings {
+        
+        static let thankYouString = "MUITO OBRIGADO!"
+        static let doneButtonTitle = "PRONTO, TERMINEI"
+        static let goodFeedbackString = "DO QUE VOCÊ MAIS GOSTOU?"
+        static let badFeedbackString = "O QUE ACONTECEU?"
+        
+        static let goodFeedbackOptions = ["Responde rápido", "Amei o produto", "Chegou no prazo", "Muito atenciosos"]
+        static let badFeedbackOptions = ["Demorou muito", "Veio estragado", "Não responderam", "Pedido errado"]
+    }
     
     @IBOutlet weak var imageView: UIImageView!
     @IBOutlet weak var thankYouLabel: UILabel!
@@ -21,7 +30,8 @@ class Rating3ViewController: UIViewController {
     @IBOutlet var starImageViews: [UIImageView]!
     @IBOutlet var feedbackButtons: [UIButton]!
     
-    @IBOutlet var doneButton: UIButton!
+    @IBOutlet weak var doneButton: UIButton!
+    @IBOutlet weak var feedbackAskLabel: UILabel!
     
     var info: Info? {
         didSet {
@@ -47,7 +57,7 @@ class Rating3ViewController: UIViewController {
         for button in feedbackButtons {
             button.layer.cornerRadius = 4.0
         }
-        
+        doneButton.setAttributedTitle(OutletStrings.doneButtonTitle.with(characterSpacing: 1.59, color: .white), for: .normal)
         
         // Do any additional setup after loading the view.
     }
@@ -59,20 +69,56 @@ class Rating3ViewController: UIViewController {
     
     func updateInfo() {
         
-        thankYouLabel.attributedText = Rating3ViewController.thankYouString.with(characterSpacing: 1.6)
+        thankYouLabel.attributedText = OutletStrings.thankYouString.with(characterSpacing: 1.6)
         ratingLabel.attributedText = ratingString?.with(characterSpacing: 2.6)
         
         if let rating = info?.rating {
-            for i in 0..<starImageViews.count {
-                
-                let star = starImageViews[i]
-                
-                star.isHidden = i >= rating
-            }
+            updateStars(rating: rating)
+            updateFeedbackAskLabel(rating: rating)
+            updateFeedbackButtons(rating: rating)
+        }
+        
+    }
+    
+    func updateStars(rating: Int) {
+        for i in 0..<starImageViews.count {
+            
+            let star = starImageViews[i]
+            
+            star.isHidden = i >= rating
         }
     }
     
-    @IBAction func donoButtonAction(_ sender: UIButton) {
+    func updateFeedbackAskLabel(rating: Int) {
+        
+        let attributedText: NSAttributedString
+        
+        if rating >= 3 {
+            attributedText = OutletStrings.goodFeedbackString.with(characterSpacing: 2.78)
+        } else {
+            attributedText = OutletStrings.badFeedbackString.with(characterSpacing: 2.78)
+        }
+        
+        feedbackAskLabel.attributedText = attributedText
+    }
+    
+    func updateFeedbackButtons(rating: Int) {
+        
+        let feedbackOptions = rating >= 3 ? OutletStrings.goodFeedbackOptions : OutletStrings.badFeedbackOptions
+        
+        for enumeration in feedbackButtons.enumerated() {
+            
+            let button = enumeration.element
+            button.setTitle(feedbackOptions[enumeration.offset], for: .normal)
+        }
+    }
+    
+    @IBAction func doneButtonAction(_ sender: UIButton) {
         doneButtonHandler?(sender)
+    }
+    
+    @IBAction func feedbackButtonAction(_ sender: UIButton) {
+        
+        let index = feedbackButtons.index(of: sender)
     }
 }
